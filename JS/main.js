3afff0dbfd9d60a13983c06416f3d2edd7e43bf1
Lucalorenzo04 <luca.lorenzo004@gmail.com>
2023-05-17 12:01:26 +0200

    Pagination completata

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,10 @@
 
 let tipoRicerca = 2;
-const api_url_getall = "https://www.eporner.com/api/v2/video/search/?per_page=50&order=latest&lq=0&format=json&gay=0&per_page=60";
-const api_url_onlyBest = "https://www.eporner.com/api/v2/video/search/?page=1&order=top-weekly&lq=0&format=json&per_page=60";
-const api_url_search = "https://www.eporner.com/api/v2/video/search/?page=1&lq=0&format=json&per_page=60&query=";
-const api_url_search_section = "https://www.eporner.com/api/v2/video/search/?per_page=60&format=json&page=1&query=";
+let pagina = 1;
+let api_url_getall = "https://www.eporner.com/api/v2/video/search/?order=latest&lq=0&format=json&gay=0&per_page=36";
 const btn = document.getElementById('cerca');
+const btnNext = document.getElementById("next");
+const btnPrev = document.getElementById('previous');
 const selectCategoria = document.getElementById('categoria');
 const search = document.getElementById('ricerca');
 const selectDurata = document.getElementById('durata');
@@ -13,11 +13,20 @@ let intestazione = document.getElementById("intestazione");
 if (btn) {
     btn.addEventListener("click", Ricerca);
 }
+
+if (btnPrev) {
+    btnPrev.addEventListener("click", prev);
+}
+
+if (btnNext) {
+    btnNext.addEventListener("click", next);
+}
 function SwitchInputSelect(num) {
     switch (num) {
         case 1:
             //Filtro Categorie
             tipoRicerca = 1;
+            pagina = 1
             selectCategoria.className = "form-select";
             selectSezione.className = "form-select visually-hidden";
             search.className = "form-control me-2 visually-hidden";
@@ -26,18 +35,20 @@ function SwitchInputSelect(num) {
         case 2:
             //Filtro Parola Chiave (Default)
             tipoRicerca = 2;
+            pagina = 1
             selectCategoria.className = "form-select visually-hidden";
             search.className = "form-control me-2";
             selectSezione.className = "form-select visually-hidden";
             search.placeholder = "Cerca";
             selectDurata.className = "form-select visually-hidden";
 
-            
+
             break;
 
         case 3:
             //Filtro Durata
             tipoRicerca = 3;
+            pagina = 1
             selectCategoria.className = "form-select visually-hidden";
             selectSezione.className = "form-select visually-hidden";
             search.className = "form-control me-2 visually-hidden";
@@ -46,6 +57,7 @@ function SwitchInputSelect(num) {
         case 4:
             //Filtro Sezione
             tipoRicerca = 4;
+            pagina = 1
             selectSezione.className = "form-select";
             search.className = "form-control me-2 visually-hidden";
             selectCategoria.className = "form-select visually-hidden";
@@ -59,13 +71,14 @@ function SwitchInputSelect(num) {
 }
 
 function Ricerca() {
+
     switch (tipoRicerca) {
         case 1:
             console.log("Ricerca per categoria");
             let categoria = document.getElementById("categoria").value;
             intestazione.innerHTML = "";
             console.log(categoria);
-            fetch(api_url_search + categoria, {
+            fetch("https://www.eporner.com/api/v2/video/search/?page=" + pagina + "&lq=0&format=json&per_page=36&query=" + categoria, {
                 "method": "GET",
                 "headers": {
                     "Accept": "application/json"
@@ -74,6 +87,7 @@ function Ricerca() {
                 .then(response => response.json())
                 .then(result => { stampaCards(result) })
                 .catch(error => console.log('Error:', error));
+
 
             break;
         case 2:
@@ -83,7 +97,7 @@ function Ricerca() {
             let key_word = document.getElementById("ricerca").value;
             intestazione.innerHTML = "Ricerca per <span id='ricercaSpan'>" + key_word + "</span>";
             console.log(key_word);
-            fetch(api_url_search + key_word, {
+            fetch("https://www.eporner.com/api/v2/video/search/?page=" + pagina + "&lq=0&format=json&per_page=36&query=" + key_word, {
                 "method": "GET",
                 "headers": {
                     "Accept": "application/json"
@@ -92,9 +106,6 @@ function Ricerca() {
                 .then(response => response.json())
                 .then(result => { stampaCards(result) })
                 .catch(error => console.log('Error:', error));
-            document.getElementById("ricerca").value = "";
-
-
             break;
         case 3:
             console.log("Ricerca per Durata");
@@ -107,7 +118,7 @@ function Ricerca() {
             }
 
             console.log(time);
-            fetch("https://www.eporner.com/api/v2/video/search/?page=1&order=" + time + "&lq=0&format=json", {
+            fetch("https://www.eporner.com/api/v2/video/search/?page=" + pagina + "&order=" + time + "&lq=0&format=json", {
                 "method": "GET",
                 "headers": {
                     "Accept": "application/json"
@@ -124,7 +135,7 @@ function Ricerca() {
             let sezione = document.getElementById("sezione").value;
             console.log(sezione);
             if (sezione == "etero") {
-                fetch(api_url_getall, {
+                fetch("https://www.eporner.com/api/v2/video/search/?order=latest&lq=0&format=json&gay=0&per_page=36&page=" + pagina, {
                     "method": "GET",
                     "headers": {
                         "Accept": "application/json",
@@ -135,7 +146,7 @@ function Ricerca() {
                     .catch(error => console.log('Error:', error));
 
             } else {
-                fetch(api_url_search_section + sezione, {
+                fetch("https://www.eporner.com/api/v2/video/search/?page=" + pagina + "&per_page=36&format=json&query=" + sezione, {
                     "method": "GET",
                     "headers": {
                         "Accept": "application/json",
@@ -157,9 +168,13 @@ function stampaCards(result) {
     console.log(result);
     let arrayVideo = result.videos;
     let cardsVideo = document.getElementById('video');
-    cardsVideo.innerHTML = "";
-    for (let i = 0; i < arrayVideo.length; i++) {
-        cardsVideo.innerHTML += `<div class="col">
+    if (arrayVideo.length == 0) {
+        intestazione.innerHTML = "Nessun risultato";
+        cardsVideo.innerHTML = "";
+    } else {
+        cardsVideo.innerHTML = "";
+        for (let i = 0; i < arrayVideo.length; i++) {
+            cardsVideo.innerHTML += `<div class="col">
         <div class="card" onclick="window.open('${arrayVideo[i].embed}')">
           <img src='${arrayVideo[i].default_thumb.src}' class="card-img-top" window.open('${arrayVideo[i].embed}')">
           <div class="card-description">
@@ -170,12 +185,14 @@ function stampaCards(result) {
           
         </div>
       </div>`;
+        }
     }
 }
 
 function CreaHome() {
     console.log("Crea Home");
-    fetch(api_url_search, {
+    tipoRicerca = 5;
+    fetch("https://www.eporner.com/api/v2/video/search/?format=json&lq=0&page=" + pagina + "&per_page=42", {
         "method": "GET",
         "headers": {
             "Accept": "application/json"
@@ -184,11 +201,13 @@ function CreaHome() {
         .then(response => response.json())
         .then(result => { stampaCards(result) })
         .catch(error => console.log('Error:', error));
+    window.scrollTo(0, 0);
 }
 
 function CreaTrending() {
     console.log("Crea Trending");
-    fetch(api_url_onlyBest, {
+    tipoRicerca = 6;
+    fetch("https://www.eporner.com/api/v2/video/search/?page=" + pagina + "&order=top-weekly&lq=0&format=json&per_page=36", {
         "method": "GET",
         "headers": {
             "Accept": "application/json"
@@ -205,30 +224,176 @@ function stampaTitolo(testo, numeroParole) {
     return paroleDaStampare;
 }
 
-categoria.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      btn.click();
-    }
-  });
+function next() {
+    window.scrollTo(0, 0);
+    switch (tipoRicerca) {
+        case 1:
+            if (pagina > 0 && pagina < 100) {
+                pagina++;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
 
-  search.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      btn.click();
-    }
-  });
+        case 2:
+            if (pagina > 0 && pagina < 100) {
+                pagina++;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
+        case 3:
+            if (pagina > 0 && pagina < 100) {
+                pagina++;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
+        case 4:
+            if (pagina > 0 && pagina < 100) {
+                pagina++;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
 
-  selectDurata.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      btn.click();
-    }
-  });
+            case 5:
+                if (pagina > 0 && pagina < 100) {
+                    pagina++;
+                } else {
+                    pagina = 1;
+                }
+                console.log(pagina);
+                intestazione.innerHTML = "Pagina " + pagina;
+                CreaHome();
+            break;
 
-  selectSezione.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      btn.click();
+        case 6:
+
+        if (pagina > 0 && pagina < 100) {
+            pagina++;
+        } else {
+            pagina = 1;
+        }
+        console.log(pagina);
+        intestazione.innerHTML = "Pagina " + pagina;
+        CreaTrending();
+
+        default:
+            break;
     }
-  });
+
+}
+
+function prev() {
+    window.scrollTo(0, 0);
+    switch (tipoRicerca) {
+        case 1:
+            if (pagina > 0 && pagina < 100) {
+                pagina--;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
+
+        case 2:
+            if (pagina > 0 && pagina < 100) {
+                pagina--;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
+        case 3:
+            if (pagina > 0 && pagina < 100) {
+                pagina--;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
+        case 4:
+            if (pagina > 0 && pagina < 100) {
+                pagina--;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            Ricerca();
+            break;
+
+            case 5:
+                if (pagina > 0 && pagina < 100) {
+                    pagina--;
+                } else {
+                    pagina = 1;
+                }
+                console.log(pagina);
+                intestazione.innerHTML = "Pagina " + pagina;
+                CreaHome();
+            break;
+
+        case 6:
+            if (pagina > 0 && pagina < 100) {
+                pagina--;
+            } else {
+                pagina = 1;
+            }
+            console.log(pagina);
+            intestazione.innerHTML = "Pagina " + pagina;
+            CreaTrending();
+            break;
+
+        default:
+            break;
+    }
+}
+
+categoria.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        btn.click();
+    }
+});
+
+search.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        btn.click();
+    }
+});
+
+selectDurata.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        btn.click();
+    }
+});
+
+selectSezione.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        btn.click();
+    }
+});
+
